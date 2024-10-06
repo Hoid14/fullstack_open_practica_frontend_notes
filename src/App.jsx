e2f59cd9c64fe82708b68e9a43ff34c81a01f6f9
Sergio Arias <s-andres140@hotmail.com>
2024-10-06 11:27:05 -0500

    hasta aqui funciona pero no agrega nuevas notas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteservice from './services/notes'
+import loginService from './services/login'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 
@@ -9,6 +10,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null) //guarda un objeto que tiene el token, username y name
 
   useEffect(() => {
     noteservice
@@ -17,6 +21,25 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+  const handleLogin = async (event) => {
+    //evita que se recargue la pagina
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user) //guarda el objeto con el token, username y name
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   const toggleImportanceof = (id) => {
     const note = notes.find(n=>n.id ===id)
@@ -62,12 +85,55 @@ const App = () => {
   ? notes
   : notes.filter(note => note.important ===true)
   
+  const loginForm = () => ( // cuando pongo parentesis, no hay necesidad de poner return
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
   
   
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {/* se utiliza para representar los formularios de manera condicional */}
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged-in</p> {/*si el usuario esta loggeado muestra su nombre en la pantalla que ya viene en el estado user*/}
+          {noteForm()} {/*muestra el formulario para ingresar notas */}
+        </div>
+      }
+      
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -84,13 +150,6 @@ const App = () => {
         ))}
         
       </ul>
-      <form onSubmit={addNote}>
-        <input 
-          value= {newNote}
-          onChange={handleNoteChange}  
-        />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   )
