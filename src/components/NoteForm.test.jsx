d4b9f0ffd117a1a403f7ad6e3c97f5731af2114f
Sergio Arias <s-andres140@hotmail.com>
2024-11-06 06:12:14 -0500

    update

--- src/components/NoteForm.test.jsx
+++ src/components/NoteForm.test.jsx
@@ -2,17 +2,16 @@ import { render, screen } from '@testing-library/react'
 import NoteForm from './NoteForm'
 import userEvent from '@testing-library/user-event'
 
-test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+test('<NoteForm /> updates parent state and calls onSubmit', () => {
   const createNote = vi.fn()
-  const user = userEvent.setup()
 
   render(<NoteForm createNote={createNote} />)
 
-  const inputs = screen.getAllByRole('textbox')
+  const input = screen.getByPlaceholderText('write note content here')
   const sendButton = screen.getByText('save')
 
-  await user.type(inputs[0], 'testing a form...')
-  await user.click(sendButton)
+  userEvent.type(input, 'testing a form...')
+  userEvent.click(sendButton)
 
   console.log(createNote.mock.calls)
 
